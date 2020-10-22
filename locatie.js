b4f6bd014e488d4f53d74b8f8a8928466988f3f4
alincorpodean <72561107+alincorpodean@users.noreply.github.com>
2020-10-22 19:16:28 +0300

    Update locatie.js

--- locatie.js
+++ locatie.js
@@ -6,7 +6,7 @@ function on_success(position){
 	document.getElementById("altitude").innerHTML = "Altitude = " + position.coords.altitude;
 	var latlon = position.coords.latitude + "," + position.coords.longitude;
 	
-	var img_url = "https: //maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&key=AIzaSyABzgIgAbsUBc3yn5l8xyBwsaOvvZdKUx4";
+	var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&key=AIzaSyABzgIgAbsUBc3yn5l8xyBwsaOvvZdKUx4";
 	
 	document.getElementById("map").innerHTML = "<img src='"+img_url+"'>";
 }
